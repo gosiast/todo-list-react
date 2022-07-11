a9f7caeeb0bdf204071f071f49a7503e4ed68011
Malgorzata Stano <stano.malgorzata@gmail.com>
2022-07-11 20:13:44 +0200

    Users can use the form to add new task.
    completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from "./logo.svg";
+/**
+ * Class component example
+ *
+ * on this TODO list we're gonna use the *class* component,
+ * the old-fashioned style of React
+ */
+
+import React from "react";
 import "./App.css";
 import TodoItem from "./components/TodoItem";
+import TodoForm from "./components/TodoForm";
 
-const state = {
-	todos: [
-		{ id: 1, task: "Walk the dog", done: false },
-		{ id: 2, task: "Water the flower", done: true },
-	],
-};
+class App extends React.Component {
+	constructor(props) {
+		super(props);
 
-function mappingEachItem(todo) {
-	return <TodoItem task={todo.task} />;
-}
+		this.state = {
+			todos: [
+				{ id: 1, task: "Walk the dog", done: false },
+				{ id: 2, task: "Water the flower", done: true },
+			],
+		};
+	}
 
-function App() {
-	return (
-		<div className="App">
-			The app is here
-			<ul>{state.todos.map(mappingEachItem)}</ul>
-		</div>
-	);
+	// this way we can modify the state of the component
+	// by using setState method from "this"
+	// handleChangeTitle() {
+	// 	this.setState({
+	//  title: "This is a new title",
+	// 	});
+	// }
+
+	onReturnedTask = (task) => {
+		const quantitityOfTodos = this.state.todos.length + 1;
+		const newTask = {
+			id: quantitityOfTodos,
+			task: task,
+			done: false,
+		};
+
+		const newTodos = this.state.todos.map((todo) => todo);
+		newTodos.push(newTask);
+
+		this.setState({
+			todos: newTodos,
+		});
+	};
+
+	render() {
+		// destructuring of an object https://www.w3schools.com/react/react_es6_destructuring.asp
+		const { todos } = this.state;
+
+		return (
+			<div className="app--wrapper">
+				<div className="app--container">
+					<h2>TO-DO list</h2>
+
+					<ul>
+						{todos.map((todo) => (
+							<TodoItem task={todo.task} />
+						))}
+					</ul>
+					<TodoForm returnedTask={this.onReturnedTask} />
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
---
+++ src/components/TodoForm.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+class TodoForm extends React.Component {
+	state = {
+		text: "",
+	};
+
+	handleOnChangeInput = (event) => {
+		const letterPressed = event.target.value;
+		this.setState({
+			text: letterPressed,
+		});
+	};
+
+	handleOnClickAddButton = (event) => {
+		event.preventDefault();
+
+		if (this.state.text !== "") {
+			this.props.returnedTask(this.state.text);
+			this.setState({
+				text: "",
+			});
+		}
+	};
+
+	render() {
+		return (
+			<form>
+				<input
+					type="text"
+					value={this.state.text}
+					onChange={this.handleOnChangeInput}
+				/>
+				<button type="submit" onClick={this.handleOnClickAddButton}>
+					Add
+				</button>
+			</form>
+		);
+	}
+}
+
+export default TodoForm;
