353dd1c1dab866cdab60bfe249ba30ebd0adb1f0
Agustin Castiarena <castiarena@gmail.com>
2022-07-11 14:03:14 +0200

    todo item component with props

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
-import logo from "./logo.svg";
 import "./App.css";
+import TodoItem from "./components/TodoItem";
+
+
+const state = {
+	todos: [
+		{ id: 1, task: 'Walk the dog', done: false },
+		{ id: 2, task: 'Water the flower', done: true }
+	]
+}
+
+function mappingEachTodo(todo) {
+	return (
+		<TodoItem task={todo.task} />
+	)
+}
 
 function App() {
 	return (
-		<div className="App">
+		<div className="app--wrapper">
+			<div className="app--container">
+				Todo list app is gonna be here
+
+				<ul>
+					{state.todos.map(mappingEachTodo)}
+				</ul>
+			</div>
+		</div>
 	);
 }
 
